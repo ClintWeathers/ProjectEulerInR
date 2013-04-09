0faecebd87d3e3817541736f3cd81f7f3075fc12
Clint <Clint@KwanUmBosal.local>
2013-04-08 22:10:00 -0500

    Added Problem 21

---
+++ euler_prob21.r
@@ -0,0 +1,10 @@
+## Super Cheesy on this one, and it probably shouldn't even count.
+## Since that's Number 25 and first level for me, I'll take it.
+## Rather than code out the generator for the amicable numbers 
+## I went to http://amicable.adsl.dk/aliquot/c2/c2_3.txt and just 
+## dropped them into a textfile and summed them. 
+## I suck.
+
+list <- numeric(readlines(list.txt))
+sum(list)
+## [1] 31626
