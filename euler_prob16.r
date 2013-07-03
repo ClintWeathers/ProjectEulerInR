aea2bc19b5d49303b9e86f956feeef6f9b42dbaf
Clint Weathers <clintweathers@gmail.com>
2013-07-03 12:36:32 -0500

    Update euler_prob16.r

--- euler_prob16.r
+++ euler_prob16.r
@@ -2,7 +2,7 @@
 #Sum of the digits of 2^1000
 #Done two ways: For clarity and for pretty. You get to pick which you like best.
 #Unfortunately, both require the GMP library in R since it freaks right the hell
-#out on big numbers. I'm still not sure where the hell that NA creeped in from. 
+#out on big numbers. I'm still not sure where that NA creeped in from. 
 
 library(gmp)
 x <- as.bigz(2**1000)
